9f7e001641265f5c010a4fa6044f3f1980a00d29
4dreamsoldier4 <88022167+4dreamsoldier4@users.noreply.github.com>
2021-11-19 17:36:27 +0300

    Update GameCalculator.java

--- src/GameCalculator.java
+++ src/GameCalculator.java
@@ -1,7 +1,7 @@
 public  abstract class GameCalculator {
     public abstract   void  hesapla();
 
-//hesaplaya gelicek olursak kim kullnıyosa herkes overriding etmek zorunda.
+
 
     public  final void  gameOver(){
         System.out.println("oyun bitti");
@@ -9,4 +9,3 @@ public  abstract class GameCalculator {
     }
 
 }
-//final overriding methodunun kullanılmasını engeller her oyunvu game over olucak anlamında.
